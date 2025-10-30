5d42b5d09813753b4640471272bc255140101c25
Behzad <behzad.piri70@gmail.com>
2025-10-30 22:55:51 +0330

    edit husck

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <p>first page</p>
-    </div>
-  );
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+
+            text
+        </div>
+    );
 }
